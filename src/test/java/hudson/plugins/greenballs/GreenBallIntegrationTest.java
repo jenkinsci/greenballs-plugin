c96ddd4ecdd8ad6fe2622744a655f7baa8056419
Asgeir Storesund Nilsen <asgeir@twingine.no>
2011-01-06 20:19:58 +0100

    Bring back old green color and bump pom version to snapshot.

--- src/test/java/hudson/plugins/greenballs/GreenBallIntegrationTest.java
+++ src/test/java/hudson/plugins/greenballs/GreenBallIntegrationTest.java
@@ -40,7 +40,7 @@ public class GreenBallIntegrationTest extends HudsonTestCase {
         final URL url = new URL(join(wc.getContextPath(), "images/48x48/blue.png"));
         final WebResponse webResponse = wc.getPage(url).getWebResponse();
         String digest = hash("SHA-1", webResponse.getContentAsBytes());
-        assertEquals("Content does not match expected digest", "05341c6c8059875d107199063ed0b9422d748cf8", digest);
+        assertEquals("Content does not match expected digest", "d2ab170a1f45dd6cf1e1580d0422abc88d6d9006", digest);
         assertTrue("Cache-Control header missing",
                 webResponse.getResponseHeaderValue("Cache-Control").contains("s-maxage"));
         assertTrue("Response has expired",
