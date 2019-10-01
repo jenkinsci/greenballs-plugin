488663bc37963a68681c33fe0a984a0e25c552b2
Raihaan Shouhell <raihaan.shouhell@autodesk.com>
2019-10-01 15:45:34 +0800

    Refresh pom and cleanup tests

--- src/test/java/hudson/plugins/greenballs/GreenBallFilterTest.java
+++ src/test/java/hudson/plugins/greenballs/GreenBallFilterTest.java
@@ -6,47 +6,67 @@ import static org.junit.Assert.assertThat;
 
 import java.util.regex.Matcher;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class GreenBallFilterTest {
 
-    GreenBallFilter greenBallFilter;
-
-    @Before
-    public void setup() {
-        greenBallFilter = new GreenBallFilter();
-    }
-
-    @Test
-    public void patternShouldMatch() {
-        final Matcher m = greenBallFilter.patternBlue.matcher("/nocacheImages/48x48/blue.gif");
+    static void patternMatchGif(Matcher m) {
         assertThat(m.find(), is(true));
         assertThat(m.group(1), equalTo("48x48"));
         assertThat(m.group(2), equalTo(""));
         assertThat(m.group(3), equalTo("gif"));
     }
 
-    @Test
-    public void patternShouldMatchPNG() {
-        final Matcher m = greenBallFilter.patternBlue.matcher("/nocacheImages/48x48/blue.png");
+    static void patternMatchPng(Matcher m) {
         assertThat(m.find(), is(true));
         assertThat(m.group(1), equalTo("48x48"));
         assertThat(m.group(2), equalTo(""));
         assertThat(m.group(3), equalTo("png"));
     }
 
-    @Test
-    public void patternShouldAlsoMatch() {
-        final Matcher m = greenBallFilter.patternBlue.matcher("/nocacheImages/48x48/blue_anime.gif");
+    static void patternMatchAnime(Matcher m) {
         assertThat(m.find(), is(true));
         assertThat(m.group(1), equalTo("48x48"));
         assertThat(m.group(2), equalTo("_anime"));
     }
 
     @Test
+    public void patternShouldMatch() {
+        final Matcher m = GreenBallFilter.patternBlue.matcher("/nocacheImages/48x48/blue.gif");
+        final Matcher m2 = GreenBallFilter.patternYellow.matcher("/nocacheImages/48x48/yellow.gif");
+        final Matcher m3 = GreenBallFilter.patternRed.matcher("/nocacheImages/48x48/red.gif");
+        patternMatchGif(m);
+        patternMatchGif(m2);
+        patternMatchGif(m3);
+    }
+
+    @Test
+    public void patternShouldMatchPNG() {
+        final Matcher m = GreenBallFilter.patternBlue.matcher("/nocacheImages/48x48/blue.png");
+        final Matcher m2 = GreenBallFilter.patternYellow.matcher("/nocacheImages/48x48/yellow.png");
+        final Matcher m3 = GreenBallFilter.patternRed.matcher("/nocacheImages/48x48/red.png");
+        patternMatchPng(m);
+        patternMatchPng(m2);
+        patternMatchPng(m3);
+    }
+
+    @Test
+    public void patternShouldAlsoMatch() {
+        final Matcher m = GreenBallFilter.patternBlue.matcher("/nocacheImages/48x48/blue_anime.gif");
+        final Matcher m2 = GreenBallFilter.patternYellow.matcher("/nocacheImages/48x48/yellow_anime.gif");
+        final Matcher m3 = GreenBallFilter.patternRed.matcher("/nocacheImages/48x48/red_anime.gif");
+        patternMatchAnime(m);
+        patternMatchAnime(m2);
+        patternMatchAnime(m3);
+    }
+
+    @Test
     public void patternShouldNotMatch() {
-        final Matcher m = greenBallFilter.patternBlue.matcher("/nocacheImages/48x48/red_anime.gif");
+        final Matcher m = GreenBallFilter.patternBlue.matcher("/nocacheImages/48x48/red_anime.gif");
         assertThat(m.find(), is(false));
+        final Matcher m2 = GreenBallFilter.patternYellow.matcher("/nocacheImages/48x48/blue_anime.gif");
+        assertThat(m2.find(), is(false));
+        final Matcher m3 = GreenBallFilter.patternRed.matcher("/nocacheImages/48x48/yello_anime.gif");
+        assertThat(m3.find(), is(false));
     }
 }
